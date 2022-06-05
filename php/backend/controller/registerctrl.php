7827e178575aaccc1400f1768c27a77b15392fb5
vali <alp-eren.42@outlook.com>
2022-06-05 20:13:14 +0200

    release 0.7.2
    reg login now functs
    error handling not working yet

--- php/backend/controller/registerctrl.php
+++ php/backend/controller/registerctrl.php
@@ -1,6 +1,9 @@
 <?php
+include('../model/registermodel.php');
+
 class registerctrl extends registermodel {
     function regctrl() {
+
         if (isset($_POST["reg-btn"])) {
             //Getting Psot Values
             $bname = $_POST['reg-bname'];
@@ -9,57 +12,36 @@ class registerctrl extends registermodel {
             $email = $_POST['reg-email'];
             $pass = $_POST['reg-pass'];
 
-            $regmodel = new registermodel();
-            $result = $regmodel->reg($bname, $nname, $vname, $email, $pass);
-
-            if (empty($_POST[$bname]) || empty($_POST[$nname]) || empty($_POST($vname)) || empty($_POST($email)) || empty($_POST($pass))) {
+            if (empty($bname) || empty($nname) || empty($vname) || empty($email) || empty($pass)) {
                 echo '<script>alert("Alle Felder müssen ausgefüllt sein!")</script>';
                 exit();
             }
+
+            elseif(!preg_match("/^[a-z\d_]{6,}$/i", $bname)) {
+                echo "<script> alert('Error (Benutzername): Nur 6-45 Zeichen, a-z bzw. A-Z, Zahlen und _ erlaubt!')</script>";
+                exit();
+            }
+
+            elseif (!preg_match("/(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]/", $nname)) {
+                echo "<script> alert('Error (Nachname): Nur a-z bzw. A-Z erlaubt!')</script>";
+                exit();
+            }
+
+            elseif (!preg_match("/(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]/", $vname)) {
+                echo "<script> alert('Error (Vorname): Nur a-z bzw. A-Z erlaubt!')</script>";
+                exit();
+            }
+
+            elseif (!preg_match("/(?=^.{6,}$)((?=.*\d+)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/", $pass)) {
+                echo "<script> alert('Error (Passwort): Nur ASCII-Character von Tastaturen und 6-255 Zeichen erlaubt! Mind. eine Zahl, ein spezielles Zeichen und ein Gross- bzw. ein Kleinbuchstabe erforderlich!')</script>";
+                exit();
+            }
+
             else {
-                if(isset ($bname) && !empty(trim($bname)) && strlen(trim($bname)) <= 45) {
-                    $bname_trim = trim($bname);
-                    // entspricht der benutzername unseren vogaben (minimal 6 Zeichen, Gross- und Kleinbuchstaben)
-                    if(!preg_match("/^[a-z\d_]{6,}$/i", $bname_trim)) {
-                        echo "Error (Benutzername): Nur 6-45 Zeichen, a-z bzw. A-Z, Zahlen und _ erlaubt!";
-                    }
-                } else {
-                    // Ausgabe Fehlermeldung
-                    echo "Bitte verwenden Sie einen gültigen Benutzernamen!";
-                }
-
-                if(isset($nname) && !empty(trim($nname)) && strlen(trim($nname)) <= 100) {
-                    $nname_trim = trim($nname);
-                    // entspricht der benutzername unseren vogaben (minimal 6 Zeichen, Gross- und Kleinbuchstaben)
-                    if(!preg_match("/(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]{6,}/", $nname_trim)){
-                        echo "Error (Nachname): Nur 6-100 Zeichen und a-z bzw. A-Z erlaubt!";
-                    }
-                } else {
-                    // Ausgabe Fehlermeldung
-                    echo "Bitte verwenden Sie einen gültigen Nachnamen!";
-                }
-
-                if(isset($vname) && !empty(trim($vname)) && strlen(trim($vname)) <= 100) {
-                    $vname_trim = trim($vname);
-                    // entspricht der benutzername unseren vogaben (minimal 6 Zeichen, Gross- und Kleinbuchstaben)
-                    if(!preg_match("/(?=.*[a-z])(?=.*[A-Z])[a-zA-Z]{6,}/", $vname_trim)){
-                        echo "Error (Vorname): Nur mind. 6 Zeichen und a-z bzw. A-Z erlaubt!";
-                    }
-                } else {
-                    // Ausgabe Fehlermeldung
-                    echo "Bitte verwenden Sie einen gültigen Vornamen!";
-                }
-
-                if(isset($pass) && !empty(trim($pass)) && strlen(trim($pass)) <= 255) {
-                    // entspricht das passwort unseren vorgaben? (minimal 8 Zeichen, Zahlen, Buchstaben, keine Zeilenumbrüche, mindestens ein Gross- und ein Kleinbuchstaben)
-                    if(!preg_match("/(?=^.{6,}$)((?=.*\d+)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/", $pass)){
-                        echo "Error (Passwort): Nur ASCII-Character von Tastaturen und 6-255 Zeichen erlaubt! Mind. eine Zahl, ein spezielles Zeichen und ein Gross- bzw. ein Kleinbuchstabe erforderlich!";
-                        exit();
-                    }
-                } else {
-                    // Ausgabe Fehlermeldung
-                    echo "Bitte verwenden Sie ein gültiges Passwort!";
-                }
+                $regmodel = new registermodel();
+                $result = $regmodel->reg($bname, $nname, $vname, $email, $pass);
+                echo '<script>alert("Erfolgreich registriert!")</script>';
+                header("location: ../../frontend/login/register.php?erfolgreich_registriert");
             }
         }
     }
--- php/backend/model/registermodel.php
+++ php/backend/model/registermodel.php
@@ -1,61 +1,17 @@
 <?php
 include('../dbc.php');
 
-use Exception;
-use App\Utility;
-
 class registermodel extends dbc {
-    private static $_inputs = [
-        "nachname" => [
-            "required" => true
-        ],
-        "vorname" => [
-            "required" => true
-        ],
-        "email" => [
-            "filter" => "email",
-            "required" => true,
-            "unique" => "benutzer"
-        ],
-        "passwort" => [
-            "min_characters" => 6,
-            "required" => true
-        ],
-    ];
-
     protected function reg($bname, $nname, $vname, $email, $pass) {
-        if (!Utility\Input::check($_POST, self::$_inputs)) {
-            return false;
-        }
-        try {
-            // Check, ob E-Mail existiert oder nicht
-            $count = "count(*)";
-            $sql = "SELECT '$count' FROM benutzer WHERE email=?";
-            $mysqli = $this->connect();
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
+        
+        $sql = "INSERT into benutzer(benutzername,name,vorname,email,passwort)VALUES(?,?,?,?,?)";
+        $mysqli = $this->connect();
+        $pass_hash = password_hash($pass, PASSWORD_BCRYPT);
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("sssss", $bname, $nname, $vname, $email, $pass_hash);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-            // Falls E-Mail existiert
-            if ($count > 0) {
-                echo "<script>alert('Diese E-Mail existiert bereits! Versuchen Sie es mit einer anderen E-Mail oder melden Sie sich damit an.');</script>";
-            } // Falls E-Mail nicht existiert
-            else {
-                $sql = "INSERT into benutzer(benutzername,name,vorname,email,passwort)VALUES(?,?,?,?,?)";
-                $mysqli = $this->connect();
-                $pass_hash = password_hash($pass, PASSWORD_BCRYPT);
-                $stmti = $mysqli->prepare($sql);
-                $stmti->bind_param("sssss", $bname, $nname, $vname, $email, $pass_hash);
-                $stmti->execute();
-                echo "<script>alert('Erfolgreich registriert! Sie können sich nun anmelden.');</script>";
-                header('location:../../frontend/index.php?erfolgreich_registriert');
-                $result = $stmt->get_result();
-
-                return $result;
-            }
-        } catch (Exception $ex) {
-            Utility\Flash::danger($ex->getMessage());
-        }
-        return false;
+        return $result;
     }
 }
